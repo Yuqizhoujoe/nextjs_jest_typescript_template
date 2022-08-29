024363fce2e22e0958cdc4692cfef970ef98baa3
damnjoejoe <49086087+Yuqizhoujoe@users.noreply.github.com>
2022-08-28 20:33:11 -0500

    Reformat the code

--- client/state/context.tsx
+++ client/state/context.tsx
@@ -1,26 +1,10 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext } from "react";
 import { LayoutInterface } from "../../shared/modal/Common/interface";
-// @ts-ignore
-import web3 from "../ethereum/web3";
 
 const AppContext = createContext<any | null>(null);
 
 export const AppContextWrapper = ({ children }: LayoutInterface) => {
-  const [accounts, cacheAccounts] = useState([]);
-
-  useEffect(() => {
-    const fetchAccounts = async () => {
-      // @ts-ignore
-      const web3Accounts = await web3.eth.getAccounts();
-      cacheAccounts(web3Accounts);
-    };
-
-    fetchAccounts();
-  }, []);
-
-  return (
-    <AppContext.Provider value={{ accounts }}>{children}</AppContext.Provider>
-  );
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
 
 export const useAppContext = () => {
